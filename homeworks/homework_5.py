8b76e8a16fa3d72c9a6bcb4401388baeee2a47bf
syimyk04-01 <syimykiliasov097@gmail.com>
2025-08-19 14:59:59 +0600

    homework_5

--- homeworks/homework_5.py
+++ homeworks/homework_5.py
@@ -1,6 +1,15 @@
 from homeworks.distance import Distance
 
 d1 = Distance(150, 'm')
-d2 = Distance(1, 'km')
+d2 = Distance(4, 'km')
+d3 = Distance(231, 'cm')
+d4 = Distance(324, 'km')
+d5 = Distance(4000, 'm')
 
 print(d1 + d2)
+print(d5 + d1)
+print(d1 - d3)
+print(d1 + d4)
+print(d5 < d4)
+print(d4 > d2)
+print(d3 == d4)
